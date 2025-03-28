782901041620954111d05c90fc233726f955987c
uahmad300625 <umar.ahmad2242@gmail.com>
2025-03-28 00:47:37 -0500

    Created Exploding Penguins

---
+++ Assets/Basic Building Blocks/Scripts/Explosion.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    public float delay = 3f;
+    //float countdown;
+   // private bool hasExploded = false;
+    public float force = 700f;
+    public float radius = 5f;
+
+    public GameObject explosionEffect;
+    
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //countdown = delay;
+    }
+
+    // Update is called once per frame
+    /*void Update()
+    {
+        countdown -= Time.deltaTime;
+
+        if(countdown <= 0f && !hasExploded)
+        {
+            Explode();
+            hasExploded = true;
+        }
+        
+    }*/
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ball"))
+        {
+            Rigidbody rb = other.GetComponent<Rigidbody>();
+            Explode(rb);
+        }
+    }
+
+    void Explode(Rigidbody ball)
+    {
+        Instantiate(explosionEffect, transform.position, transform.rotation);
+
+        ball.AddExplosionForce(force, transform.position, radius);
+
+        Destroy(gameObject);
+
+    }
+}
