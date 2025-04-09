df9a5bdcb96f14f4e49e93529533f6c6b0faf3ef
uahmad300625 <umar.ahmad2242@gmail.com>
2025-04-08 23:00:50 -0500

    Finished most of the second course, just need to finish the ending

---
+++ Assets/Basic Building Blocks/Scripts/Breakable Floor4.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using UnityEngine;
+
+public class BreakableFloor4 : MonoBehaviour
+{
+
+    public float delay = 5f;
+    //private bool triggered = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ball"))
+        {
+            //triggered = true;
+            StartCoroutine(DisableAfterDelay());
+        }
+    }
+
+    private IEnumerator DisableAfterDelay()
+    {
+        yield return new WaitForSeconds(delay);
+        gameObject.SetActive(false);
+    }
+}
