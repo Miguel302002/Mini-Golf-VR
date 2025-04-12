9e45791294f2be4ed32915e168674a394580168c
uahmad300625 <umar.ahmad2242@gmail.com>
2025-04-12 00:32:12 -0500

    Found a way to animate the skeleton to "attack the ball" and make it lose health

---
+++ Assets/Basic Building Blocks/Scripts/DeathBarrier.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class DeathBarrier : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ball"))
+        {
+            
+            BallManager.Instance.TakeDamage(100);
+        }
+    }
+}
